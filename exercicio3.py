84726a6c9b225f2124deb45a9521940892b5418c
paulo henrrique pereira da costa <paulohenriquep424@gmail.com>
2025-03-20 14:37:42 -0400

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+numero1 = int(input("insira o primeiro numero"))
+numero2 = int(input("insira o segundo numero"))
+soma= numero1 + numero2 
+sub= numero1- numero2
+mult = numero1 * numero2
+print (f"a soma desse numeros foi {soma}")
+print (f" a subtracao desse numero foi {sub}")
+print (f" a multiplicacao desse numero foi {mult}")
