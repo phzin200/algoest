d35b54ddefd9d02b25766d19117db12bbccda04c
paulo henrrique pereira da costa <paulohenriquep424@gmail.com>
2025-03-20 18:14:52 -0400

    Add files via upload

---
+++ exer4.py
@@ -0,0 +1,5 @@
+num = int(input("insira um numero"))
+if num%2 ==0:
+    print("o numero e par")
+else:
+    print("o numero e impar")
---
+++ exer5.py
@@ -0,0 +1,8 @@
+num = int(input("insira um numero"))
+if num > 0:
+    print (f"o numero e positivo")
+elif num < 0:
+    print ( "o numero e negativo")
+else:
+    print (" o numero e zero")
+
