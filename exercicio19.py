2d6fdf6f0e99849d9de16054b2125e7f81e6d70f
paulo henrrique pereira da costa <paulohenriquep424@gmail.com>
2025-04-10 15:11:41 -0400

    Add files via upload

---
+++ exercicio19.py
@@ -0,0 +1,6 @@
+salario=input("insira seu salario")
+horas=int(input("insira a quantidade das horas"))
+valor=int(input("insirao valor das horas extras"))
+horasv=horas*valor
+soma=salario+horasv
+print(f"seu valor total do salario{soma}")
